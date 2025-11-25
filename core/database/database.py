88667ecc6fcc99231089f0dec8b9acbe204d6702
Yilin20021016 <werock2517@gmail.com>
2025-11-26 00:40:23 +0800

    refactor: refactor database class

--- core/database/database.py
+++ core/database/database.py
@@ -1,18 +1,25 @@
-from core.database.base_db import BaseDB
+import datetime
+import os
+import random
 
-class Database(BaseDB):
-    @BaseDB.db_operation
+class Database():
+    def __init__(self, db_filename="order_management.db"):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.db_path = os.path.join(base_dir, db_filename)
+
+    @staticmethod
+    def generate_order_id() -> str:
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        random_num = random.randint(1000, 9999)
+        return f"OD{timestamp}{random_num}"
+
     def get_product_names_by_category(self, cur, category):
 
-    @BaseDB.db_operation
     def get_product_price(self, cur, product):
 
-    @BaseDB.db_operation
     def add_order(self, cur, order_data):
 
-    @BaseDB.db_operation
     def get_all_orders(self, cur):
 
-    @BaseDB.db_operation
     def delete_order(self, cur, order_id):
-
